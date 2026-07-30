68330a94709e71329090b3dca56f84c9549987ff
Tlhompo Khanye <tlhompokhanye00@gmail.com>
2026-07-31 00:34:22 +0200

    Changed some function name

---
+++ Student.h
@@ -0,0 +1,33 @@
+#ifndef STUDENT
+#define STUDENT
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student{
+    private:
+    string name;
+    int studentNumber;
+    double averageMark;
+
+    public:
+    //Constructors
+    Student(string name, int studentNumber, double averageMark) : name(name), studentNumber(studentNumber), averageMark(averageMark) {}
+    Student() : name("Unknown"), studentNumber(-1), averageMark(-1) {}
+
+    //Destructor
+    ~Student(){
+        cout << "Student: " << name << " was removed from the data base.";
+    }
+
+    //Setters
+    void setName(string name);
+    void setStudentNumber(int studentNumber);
+    void setAverageMark(double averageMark);
+};
+
+
+
+
+#endif /* STUDENT */
