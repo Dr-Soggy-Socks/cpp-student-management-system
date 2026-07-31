9092a4095f65a825ba8ba654736fa5d40569707b
Tlhompo Khanye <tlhompokhanye00@gmail.com>
2026-07-31 02:07:14 +0200

    testing the empty defult constructor

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 
 int main(){
+    Student sibahle("Sibahle", 1, 80.0);
+    Student tlhompo("Tlhompo", 2, 60.0);
+    Student alice("Alice", 3, 78.6);
+    Student bob;
 
-
+    sibahle.display();
+    cout << '\n';
+    tlhompo.display();
+    cout << '\n';
+    alice.display();
+    cout << '\n';
+    bob.display();
 }
